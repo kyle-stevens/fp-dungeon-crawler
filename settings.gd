824a2a7f4a22015e0f4a17fcb41e40711ac26d0b
Kyle Stevens <43458322+kyle-stevens@users.noreply.github.com>
2023-01-31 10:48:05 -0700

    adding a main menu to be implemented

---
+++ settings.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var master_volume : int = 100
+var brightness : int = 100
+var sound_effects_volume : int = 100
+var monster_effects_volume : int = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
